540380bb46d09f2f8d3d32acbce5090676c91287
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-08 14:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
+ocument.addEventListener('DOMContentLoaded', function() {
     const squares = document.getElementsByClassName('square');
 
     // Add event listener for 'mouseover' event to each square
     Array.from(squares).forEach(square => {
         square.addEventListener('mouseover', function() {
-            // Change the color of all squares to Coffee
+            // Change the color of non-hovered squares to Coffee
             Array.from(squares).forEach(s => {
-                s.style.backgroundColor = '#6F4E37'; // Coffee
+                if (s !== square) {
+                    s.style.backgroundColor = '#6F4E37'; // Coffee
+                }
             });
         });
 
         // Add event listener for 'mouseout' event to each square
         square.addEventListener('mouseout', function() {
-            // Change the color of all squares back to Lavender
+            // Change the color of non-hovered squares back to Lavender
             Array.from(squares).forEach(s => {
-                s.style.backgroundColor = '#E6E6FA'; // Lavender
+                if (s !== square) {
+                    s.style.backgroundColor = '#E6E6FA'; // Lavender
+                }
             });
         });
     });
-});
+	   });
